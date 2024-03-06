103de96e5efb0c570403dde8143f07f2ba656f26
Ronaldo Hoch <ronaldopintohoch@gmail.com>
2024-03-06 10:17:05 -0300

    algumas mudanças

--- projects/ui/src/button/button.component.ts
+++ projects/ui/src/button/button.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
 
