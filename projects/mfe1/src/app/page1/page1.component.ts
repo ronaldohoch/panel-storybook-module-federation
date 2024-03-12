9138f44c097df996eeb3d92cc1a03936174723c1
Ronaldo Hoch <ronaldopintohoch@gmail.com>
2024-03-12 17:53:40 -0300

    usando services

--- projects/mfe1/src/app/page1/page1.component.ts
+++ projects/mfe1/src/app/page1/page1.component.ts
@@ -3,16 +3,24 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
+import { AuthService } from '@dinamize/ui';
 
 @Component({
   selector: 'mfe1-page1',
   standalone: true,
   imports: [
-    MatButtonModule,MatCardModule
+    MatButtonModule, MatCardModule
   ],
   templateUrl: './page1.component.html',
   styleUrl: './page1.component.scss'
 })
 export class Page1Component {
-
+  user:any;
+  constructor(private authSvc: AuthService) {
+    this.authSvc.user$.subscribe(data=>{
+      this.user = data;
+      console.log('user',this.user)
+    })
+    this.authSvc.userInfo().subscribe()
+  }
 }
--- projects/shared-lib/src/_services/auth.service.ts
+++ projects/shared-lib/src/_services/auth.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private userSubject = new BehaviorSubject<any>(null);
+  public user$ = this.userSubject.asObservable();
 
   constructor(private http: HttpClient) { }
 
-  userInfo(body: any): Observable<any> {
+  userInfo(): Observable<any> {
     return this.http
-      .post<any>(`http://panel.mauricio.martini.dina.local/passport/user/info`, body)
+      .post<any>(`http://panel.mauricio.martini.dina.local/passport/user/info`, {})
+      .pipe(
+        tap((data) => {
+          if(data.hasOwnProperty('hashId')){
+            this.userSubject.next(data);
+          }
+        }),
+      )
       .pipe(
         catchError((error) => {
           return throwError(() => error);
